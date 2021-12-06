8f24d870b3d8fda9d4842d3c3fd274a28081c2a9
dinaev.m@mail.ru <dinaev.m@mail.ru>
2021-12-06 21:36:28 +0300

    cli

--- gendiff/scripts/gendiff_script.py
+++ gendiff/scripts/gendiff_script.py
@@ -6,7 +6,7 @@ def main():
     first_arg = gendiff_args.args.first_file
     second_arg = gendiff_args.args.second_file
     format_style = gendiff_args.args.format
-    return generate_diff(first_arg, second_arg, format_style)
+    print(generate_diff(first_arg, second_arg, format_style))
 
 
 if __name__ == '__main__':
